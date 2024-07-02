f4148a53a667bf83de9dfdd15b815583b98fcce4
edwinrojas203 <161768088+edwinrojas203@users.noreply.github.com>
2024-07-02 13:28:06 -0500

    creada y terminada la pagina del video 11

---
+++ src/views/AutorView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="contenedor">
+        <div class="autor">
+            <img src="/imagenes/systemplus.png" alt="">
+            <p>Este proyecto fue desarrollado por:</p>
+            <h3>Santiago Rojas</h3>
+            <p>Curso Diseño Web - Systemplus - 2024</p>
+            <a href="https://www.github.com/edwinrojas203" target="_blank">edwinrojas203</a>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .contenedor{
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+   }
+   img{
+    max-width: 50%;
+   }
+   a{
+    text-decoration: none;
+    color: inherit;
+   }
+
+</style>
